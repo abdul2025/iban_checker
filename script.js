ddb9c18526656a3e8ed923c225ecf115de88269f
abdulwahab alshehri <abdul.2020alsh@gmail.com>
2024-12-23 21:27:37 +0300

    init

---
+++ script.js
@@ -0,0 +1,164 @@
+const banks = [
+    {
+        'samaCode': '55',
+        'bankName': 'Banque Saudi Fransi',
+        'nameEn': 'Banque Saudi Fransi',
+        'nameAr': 'البنك السعودي الفرنسي',
+    },
+    {
+        'bankName': 'Alrajhi Bank',
+        'samaCode': '80',
+        'nameEn': 'Alrajhi Bank',
+        'nameAr': 'بنك الراجحي',
+    },
+    {
+        'samaCode': '10',
+        'bankName': 'National Commercial Bank',
+        'nameEn': 'National Commercial Bank',
+        'nameAr': 'البنك الأهلي التجاري',
+    },
+    {
+        'samaCode': '45',
+        'bankName': 'Saudi British Bank',
+        'nameEn': 'SABB',
+        'nameAr': 'ساب',
+    },
+    {
+        'samaCode': '20',
+        'bankName': 'Riyadh Bank',
+        'nameEn': 'Riyad Bank',
+        'nameAr': 'بنك الرياض',
+    },
+    {
+        'samaCode': '40',
+        'bankName': 'Saudi American Bank',
+        'nameEn': 'SAMBA',
+        'nameAr': 'سامبا',
+    },
+    {
+        'samaCode': '05',
+        'bankName': 'Alinma Bank',
+        'nameEn': 'AL Inma Bank',
+        'nameAr': 'بنك الانماء',
+    },
+    {
+        'samaCode': '50',
+        'bankName': 'Alawwal bank',
+        'nameEn': 'AlAwwal Bank',
+        'nameAr': 'البنك الأول',
+    },
+    {
+        'samaCode': '60',
+        'bankName': 'Bank AlJazira',
+        'nameEn': 'Bank Aljazerah',
+        'nameAr': 'بنك الجزيرة',
+    },
+    {
+        'samaCode': '65',
+        'bankName': 'Saudi Investment Bank',
+        'nameEn': 'Saudi Investment Bank',
+        'nameAr': 'البنك السعودي للأستثمار',
+    },
+    {
+        'samaCode': '15',
+        'bankName': 'Bank AlBilad',
+        'nameEn': 'BANK ALBELAD',
+        'nameAr': 'بنك البلاد',
+    },
+    {
+        'samaCode': '30',
+        'bankName': 'Arab National Bank',
+        'nameEn': 'Arab National Bank',
+        'nameAr': 'البنك العربي الوطني',
+    },
+    {
+        'samaCode': '90',
+        'bankName': 'GULF Bank',
+        'sarieCode': 'GULFSARI',
+        'nameEn': 'Gulf International Bank',
+        'nameAr': 'بنك الخليج',
+    },
+    {
+        'samaCode': '95',
+        'bankName': 'Emirates Bank',
+        'nameEn': 'EMARITE BANK',
+        'nameAr': 'بنك الامارات',
+    },
+    {
+        'samaCode': '76',
+        'bankName': 'Bank Muscat',
+        'nameEn': 'Bank Muscat',
+        'nameAr': 'بنك مسقط',
+    },
+    {
+        'samaCode': '71',
+        'bankName': 'National Bank of Bahrain',
+        'nameEn': 'National Bank Of Bahrain',
+        'nameAr': 'بنك البحرين الوطني',
+    },
+    {
+        'samaCode': '75',
+        'bankName': 'National Bank of Kuwait',
+        'nameEn': 'National Bank of Kuwait',
+        'nameAr': 'بنك الكويت الوطني',
+    },
+    {
+        'samaCode': '85',
+        'bankName': 'BNP Paribas Bank',
+        'nameEn': 'BNP PARIBAS SAUDI ARABIA',
+        'nameAr': 'بي ان بي باريبوس',
+    },
+];
+
+
+
+function check_iban(iban){
+    //  There is a value
+    if(!iban){
+        return false
+    }
+    
+    // Clean spaces
+    iban = iban.replaceAll(' ', '');
+
+    // check length
+    if(iban.length !== 24){
+        return false
+    }
+    // must start with SA
+    if(!iban.startsWith('SA')){
+        return false
+    }
+
+    return iban
+}
+testIban = 'SA03 8000 0000 6080 1016 7519'
+
+function get_iban() {
+
+    let ibanField = document.getElementById('iban')
+    let iban = ibanField.value
+    let resualt = document.getElementById('resualt');
+    
+
+    
+    iban = check_iban(iban)
+    if(iban === false){
+        resualt.innerHTML = 'الايبان غير صحيح';
+
+    }else {
+        resualt.innerHTML = 'الايبان صحيح';
+
+        
+        const bankCode = iban.substring(4, 6);
+        const bankdetails = banks.find(bank => bank.samaCode === bankCode);
+
+        document.getElementById('nameAr').innerHTML = bankdetails.nameAr;
+        document.getElementById('nameEn').innerHTML = bankdetails.nameEn;
+        document.getElementById('samaCode').innerHTML = bankdetails.samaCode;
+        
+    }
+
+    // ibanField.value = ''
+    
+}
